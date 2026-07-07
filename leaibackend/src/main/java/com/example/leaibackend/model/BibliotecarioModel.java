23c9e54bdbf8f86828edce043ea41755b0de94c8
Michell Silva Santos <15558@pcivil.intranet>
2026-07-07 11:05:47 -0300

    Subindo as dependências do application.properties do backend

---
+++ leaibackend/src/main/java/com/example/leaibackend/model/BibliotecarioModel.java
@@ -0,0 +1,10 @@
+package com.example.leaibackend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tab_bibliotecario")
+public class BibliotecarioModel {
+
+}
