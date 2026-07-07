d1968d19698f17282c58e00dd3d2f0a705174130
michellfloat <michell.ssfloat@gmail.com>
2026-07-06 21:33:57 -0300

    feat:Subindo pra valer o BackEnd(espero que dessa vez dê certo namoral)

---
+++ leaibackend/src/main/java/com/example/leaibackend/LeaibackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.leaibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaibackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaibackendApplication.class, args);
+	}
+
+}
---
+++ leaibackend/src/test/java/com/example/leaibackend/LeaibackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.leaibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaibackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
