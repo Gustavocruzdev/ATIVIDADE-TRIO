9650d431cd3c79ee16a1e78b23d4d564036a802b
Michell Silva Santos <15558@pcivil.intranet>
2026-07-07 11:26:37 -0300

    feat:Subindo os Exceptions do sistema

---
+++ leaibackend/src/main/java/com/example/leaibackend/exception/BusinessException.java
@@ -0,0 +1,10 @@
+package com.example.leaibackend.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class BusinessException extends RuntimeException{
+    public BusinessException(String message){
+        super(message);
+    }
+}
